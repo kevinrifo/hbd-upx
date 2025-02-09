ee2569a8ec1a2a2b7479c70681270e8dfa3a781d
Kevin Rifo Buana <kevinrifo666@gmail.com>
2025-02-09 16:41:31 +0700

    Adjusted route names

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -4,9 +4,9 @@ import { validateUser } from "../middlewares/rules-middleware";
 
 const router = express.Router();
 
-router.delete("/users/:id", deleteUser);
-router.post("/users", validateUser, createUser);
-router.get("/users", getAllUsers);
-router.put("/users/:id", validateUser, updateUser);
+router.delete("/user/:id", deleteUser);
+router.post("/user", validateUser, createUser);
+router.get("/user", getAllUsers);
+router.put("/user/:id", validateUser, updateUser);
 
 export default router;
