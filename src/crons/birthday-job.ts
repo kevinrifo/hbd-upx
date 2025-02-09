d50be21de5beba5b7e83cd0b7a2d8b5d6e203ce8
Kevin Rifo Buana <kevinrifo666@gmail.com>
2025-02-09 16:53:51 +0700

    Optimized job logic

--- src/crons/birthday-job.ts
+++ src/crons/birthday-job.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import moment from 'moment-timezone';
 import prisma from "../configs/db";
-import { BirthdayMessageWithUser } from "../utils/constants";
+import { BirthdayMessageWithUser, sendHBDat } from "../utils/constants";
+import { delay } from "../utils/helper";
 
 async function birthDayMessage() {
-
+    console.log('Executing job')
     //Get users and unsent messages
     const [usersToWishHBD, unsentMessages] = await Promise.all([
         findMatchingBirthday(),
@@ -16,31 +17,52 @@ async function birthDayMessage() {
 
     const birthDayMessages: Array<BirthdayMessageWithUser> = [...unsentMessages];
 
-    for (const user of usersToWishHBD) {
-        // Check if message with user and matching birthday exists, if already exists dont make again
-        const existingMessage = await prisma.birthdayMessage.findFirst({
-            where: { user_id: user.id, send_date: moment(user.sendBirtdayMessageAt).utc().format("YYYY-MM-DD")}
+    const usersWithSendDate = usersToWishHBD.map(user => ({
+        user,
+        send_date: moment(user.sendBirtdayMessageAt).utc().format("YYYY-MM-DD")
+    }));
+
+    // Fetch all existing messages if exists
+    const existingMessages = await prisma.birthdayMessage.findMany({
+        where: {
+            OR: usersWithSendDate.map(({ user, send_date }) => ({
+                user_id: user.id,
+                send_date
+            }))
+        },
+        select: { user_id: true, send_date: true }
+    });
+
+    // Convert existing messages into a set for faster comparing
+    const existingUserMessages = new Set(existingMessages.map(({ user_id, send_date }) => `${user_id}-${send_date}`));
+
+    // Only makes message if theres no previous identical message
+    const messagesToBeCreated = usersWithSendDate
+        .filter(({ user, send_date }) => !existingUserMessages.has(`${user.id}-${send_date}`))
+        .map(({ user, send_date }) => ({
+            user_id: user.id,
+            sent_at: null,
+            send_date
+        }));
+
+    //Insert multiple new messages with single query
+    if (messagesToBeCreated.length > 0) {
+        await prisma.birthdayMessage.createMany({ data: messagesToBeCreated, skipDuplicates: true });
+
+        // Fetch inserted messages with user data
+        const createdMessages: Array<BirthdayMessageWithUser> = await prisma.birthdayMessage.findMany({
+            where: {
+                OR: messagesToBeCreated.map(({ user_id, send_date }) => ({
+                    user_id,
+                    send_date
+                }))
+            },
+            include: { user: true }
         });
 
-        if (!existingMessage) {
-            //Make birthday message records
-            const birthdayMessage = await prisma.birthdayMessage.create(
-                {
-                    data: {
-                        user_id: user.id,
-                        sent_at: null,
-                        send_date: moment(user.sendBirtdayMessageAt).utc().format("YYYY-MM-DD"),
-                    },
-                    include: {
-                        user: true,
-                    },
-                });
-            birthDayMessages.push(birthdayMessage)
-        } else {
-            console.log('Message already exist, going to send existing message instead if its not sent yet')
-        }
-
-    };
+        //Push created messages to existing queue, ready to send
+        birthDayMessages.push(...createdMessages)
+    }
 
     for (const birthdayMessage of birthDayMessages) {
         try {
@@ -50,18 +72,15 @@ async function birthDayMessage() {
             });
 
             if ((response.data.status) as string == "sent") {
-                // Update the sent_at so we know its already sent and wont resend at the next attempt
-                try {
-                    await prisma.birthdayMessage.update({
-                        where: { id: birthdayMessage.id },
-                        data: { sent_at: moment().utc().format() }
-                    })
-                }
-                catch (error) {
-                    console.error('Error:', error instanceof Error ? error.message : "Unknown error");
-                }
+                // Update the sent_at so we know its already sent and wont be sent again at the next attempt
+                await prisma.birthdayMessage.update({
+                    where: { id: birthdayMessage.id },
+                    data: { sent_at: moment().utc().format() }
+                })
             }
 
+            //3 secs delay for each message
+            await delay(3)
             console.log('Response:', response.data);
         } catch (error) {
             console.error('Error:', error instanceof Error ? error.message : "Unknown error");
@@ -70,7 +89,7 @@ async function birthDayMessage() {
 };
 
 const findMatchingBirthday = async (): Promise<Array<any>> => {
-    
+
     // Our server time in UTC
     const nowUtc = moment()
         .utc()
@@ -84,15 +103,15 @@ const findMatchingBirthday = async (): Promise<Array<any>> => {
       But, we need to convert their 9am to UTC first
      **/
 
-      const mappedUsers = allUsers.map(user => {
+    const mappedUsers = allUsers.map(user => {
 
-        // Edge case: et the user's current local time (which may still be in the previous year)
+        // Edge case: get the user's current local time (which may still be in the previous year)
         const userYear = moment().tz(user.timezone).year();
 
         return {
             ...user,
             sendBirtdayMessageAt: moment
-                .tz(`${userYear}-${user.birth_date.slice(5)} 09:00:00`, user.timezone) // User's 9 AM
+                .tz(`${userYear}-${user.birth_date.slice(5)} ${sendHBDat}`, user.timezone) // User's 9 AM
                 .utc()
                 .format()
         };
