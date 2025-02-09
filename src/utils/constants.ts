d3b4e9512764458b257572efd01c179485a28522
Kevin Rifo Buana <kevinrifo666@gmail.com>
2025-02-09 16:55:29 +0700

    Adjusted time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { Prisma } from "@prisma/client"
 
 export const cronRule = "* * * * *"
-export const sendHBDat = "16:51:00"
+export const sendHBDat = "09:00:00"
 export type BirthdayMessageWithUser = Prisma.BirthdayMessageGetPayload<{
     include: { user: true }
 }>
