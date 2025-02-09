f77edcbe228009c4cf679237072a9b29d0d75ed0
Kevin Rifo Buana <kevinrifo666@gmail.com>
2025-02-09 16:54:44 +0700

    Moved message sending time to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 import { Prisma } from "@prisma/client"
 
-export const cronRule = "* * * * *" 
+export const cronRule = "* * * * *"
+export const sendHBDat = "16:51:00"
 export type BirthdayMessageWithUser = Prisma.BirthdayMessageGetPayload<{
     include: { user: true }
 }>
